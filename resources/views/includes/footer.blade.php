39a5c507cd70bcf3a459a3e0ba0cf7094673c9aa
Alex <dirfentos@gmail.com>
2025-02-14 17:51:21 +0100

    02.14-ig

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,20 +1,20 @@
-<footer class="text-black py-4 bg-primary d-flex align-items-center" style="height: 100px">
+<footer class="text-black py-4  d-flex align-items-center" style="height: 100px; background-color: #003366; color: white;">
     <div class="container ">
         <div class="row">
             <div class="col-md-6">
-                <h6 class="m-1">Elérhetőségek:</h6>
+                <h6 class="m-1" style="color: white;">Elérhetőségek:</h6>
 
                 <ul>
-                    <li style="font-size:12px">9943, Kondorfa Alvég u. 1.</li>
-                    <li style="font-size:12px">+3694428013</li>
-                    <li style="font-size:12px">hivatal@kondorfa.hu</li>
+                    <li style="font-size:12px; color: white;">9943, Kondorfa Alvég u. 1.</li>
+                    <li style="font-size:12px; color: white;">+3694428013</li>
+                    <li style="font-size:12px; color: white;">hivatal@kondorfa.hu</li>
                 </ul>
                     
                
             </div>
             <div class="col-md-6 text-md-end">
-                <h6>Kövess minket</h6>
-                <a href="https://www.facebook.com/profile.php?id=100070319796824" class="text-black me-3" target="_blank"><i class="fab fa-facebook"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"></i> Facebook</a>
+                <h6 style="color: white;">Kövess minket</h6>
+                <a href="https://www.facebook.com/profile.php?id=100070319796824" class="text-black me-3" style="color: white;" target="_blank"><i class="fab fa-facebook"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" ></i> Facebook</a>
 
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,93 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <link rel="icon" href="{{ asset('images/cimer.png') }}" type="image/x-icon">
-    <style>
-        body {
-            display: flex;
-            flex-direction: column;
-            min-height: 100vh;
-        }
-
-        .content {
-            flex: 1; 
-        }
-
-        footer {          
-            height: 100px;
-        }
-
-        .position-relative {
-            position: relative;
-        }
-
-        .position-relative:hover #overlay,
-        .position-relative:hover #text-overlay {
-            display: block;
-            visibility: hidden;
-            transition: opacity 0.5s ease-in-out, visibility 0s 0.5s;
-            opacity: 0;           
-        }
-
-        .position-relative:hover #overlay,
-        .position-relative:hover #text-overlay {
-            opacity: 1;
-            visibility: visible;
-            transition: opacity 0.5s ease-in-out, visibility 0s 0s; 
-        }
-
-        #overlay, #text-overlay {
-            visibility: hidden;
-            opacity: 0;
-        }
-
-    </style>
-    
+    @include('includes.links') 
     
     <title>@yield('title')</title>
 </head>
-<body class="bg-primary bg-opacity-25">
+<body style="background-color: #b3d9ff; font-family: 'DM Serif Text', serif; text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.3);">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     @include('includes.nav')
     
-    <div class="content">
-        @yield('content')
-    </div>
+    
+     @yield('content')
+    
 
 
     @include('includes.footer')
     
-    <script>
-        // Kép és szöveg megjelenítése késleltetve
-        const imageArea = document.querySelector('.position-relative');
-        const overlay = document.getElementById('overlay');
-        const textOverlay = document.getElementById('text-overlay');
-    
-        imageArea.addEventListener('mouseenter', () => {
-            setTimeout(() => {
-                overlay.style.visibility = 'visible';
-                textOverlay.style.visibility = 'visible';
-                overlay.style.opacity = '1';
-                textOverlay.style.opacity = '1';
-            }, 500); // 500 ms késleltetés
-        });
-    
-        imageArea.addEventListener('mouseleave', () => {
-            overlay.style.opacity = '0';
-            textOverlay.style.opacity = '0';
-            overlay.style.visibility = 'hidden';
-            textOverlay.style.visibility = 'hidden';
-        });
-    </script>
-    
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+    @include('includes.scripts')
 </body>
 </html>
